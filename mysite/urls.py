8bce2999c1a4c2fd08ea6054ded1a80665a8bdcc
Tatiane <tati.s.stiv@gmail.com>
2016-06-26 19:52:38 -0300

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 import django.contrib.auth.views
 
-from django.contrib import admin
-
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'', include('blog.urls')),
-)
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+]
