06303f6e4587b0b6b5934593f7782ed0c2e33ef6
Tatiane <tati.s.stiv@gmail.com>
2016-06-26 19:45:04 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -15,6 +16,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -28,6 +30,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
+import django.contrib.auth.views
+
 from django.contrib import admin
 
-urlpatterns = [
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'', include('blog.urls')),
-]
+)
